7cd5d7bdacaec8efb46484f2f4bb923034680531
ЗОЛЗАЯА Бат-Эрдэнэ <you@example.com>
2024-11-21 14:21:25 +0800

    mongoDb connected % find()

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
+import connectDb from "./connectDB.js";
 
 dotenv.config();
 
@@ -9,11 +10,23 @@ const server = express();
 const PORT = 8000;
 
 server.use(cors());
-server.get("/", (request, response) => {
-  response.send(["Zoloo", "Zen", "Buddhism"]);
+server.post("/", async (request, response) => {
+  const db = await connectDb();
+  let collection = db.collection("movies");
+  let res = await collection.insertOne({
+    plot: "Zoloo tuguu mongodb collection ruu document-iig insertOne ashiglan oruulav",
+    genres: ["Zoloo", "Tuuguu"],
+  });
+
+  response.json({
+    success: true,
+    data: res,
+  });
+
+  // response.send(["Zoloo", "Zen", "Buddhism"]);
 });
 
-server.post("/image", async (request, response) => {
+server.post("/image-upload", async (request, response) => {
   try {
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -26,6 +39,19 @@ server.post("/image", async (request, response) => {
   } catch (error) {
     console.log("cloudinary error", error);
   }
+});
+
+server.get("/", async (request, response) => {
+  const database = await connectDb();
+  const moviesCollection = database.collection("movies");
+  const ourInsertedDocuments = await moviesCollection.find({
+    genres: ["Zoloo", "Tuuguu"],
+  });
+  await ourInsertedDocuments.next();
+  const result = await ourInsertedDocuments.next();
+  response.status(200).json({
+    result: result,
+  });
 });
 
 server.listen(PORT, () => {
